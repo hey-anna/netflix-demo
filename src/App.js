97e214ff87bff9dafe11e6e9df43e416284991db
hey-anna <106728222+hey-anna@users.noreply.github.com>
2024-04-06 22:26:14 +0900

    기본 라우터 및 페이지 세팅
    01 NavBar = AppLayout으로 name 명명
    02 폴더 구조 및 파일 등 라우더 구조를 체계화 하여 세팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import AppLayout from "./layout/AppLayout";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import MoviePage from "./pages/Movies/MoviePage";
+import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+// import HomePage from "./pages/Homepage/Homepage";
 
-// 유저는 배너를 볼 수 있다. [o]
+// 과제 리스트
+// [o]
+// 유저는 배너를 볼 수 있다.
 // 유저는 가장 인기있는 영화 리스트, 가장 평점 좋은 영화 리스트, 상영 예정작 리스트를 볼 수 있다.
 // 각각의 영화 리스트는 슬라이드 형태로 넘길 수 있다.
 // 유저는 영화에 마우스를 올려두면 영화의 제목과 장르, 평점, 인기도, 청불여부를 볼 수 있다.
@@ -12,8 +21,33 @@ import "./App.css";
 // 유저는 장르별로 영화를 필터링 할 수 있다.
 // 유저는 영화 날짜별로 필터링 할 수 있다.
 
+// 홈페이지 /
+// 영화 전체보여주는 페이지 (서치) /movies
+// 영화 디테일 페이지 /movies/:id
+// 추천 영화 /movies/:id/recommendation
+// 리뷰 /movies/:id/reviews
+
+//sub(서브) 라우트 = nested route를 넣어줄 수 있음
+
+// / index 붙이면 >> 위에거를 그대로 쓰겠다는
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="movies">
+            <Route index element={<MoviePage />} />
+            <Route path=":id" element={<MovieDetailPage />} />
+          </Route>
+          {/* <Route path="/movies" element={<MoviePage />} />
+          <Route path="/movies/:id" element={<MovieDetailPage />} /> */}
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
