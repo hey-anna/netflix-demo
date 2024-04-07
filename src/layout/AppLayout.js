5510aa62fa377034baaf0257d8cf76d601778b42
hey-anna <106728222+hey-anna@users.noreply.github.com>
2024-04-07 10:16:22 +0900

    나무위키 로고로 수정

--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -5,7 +5,8 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Outlet, Link } from "react-router-dom"; // 리액트 V6  도입 // 리액트 안에 있는 자손들을 가져오게 해주는
-import netlogo from "../assets/images/netlogo.svg";
+// import netlogo from "../assets/images/netlogo.svg";
+// import logo from "../assets/images/logo.svg";
 
 const AppLayout = () => {
   return (
@@ -25,7 +26,8 @@ const AppLayout = () => {
         >
           <Navbar.Brand href="#">
             <img
-              src={netlogo}
+              // src={logo}
+              src="https://i.namu.wiki/i/VC4ybUEcyxjcNN9bbD94MSuC90nzQOrsa-ZKYd8ZCcbFW62IB2vvvP5hZxbdNPR2oms8avAsSQJXKCQ3L4rTpw.svg"
               width="100"
               height="30"
               className="d-inline-block align-top"
