f981f0b581e52bc22bf1cddc633ce3006b2f312f
hey-anna <106728222+hey-anna@users.noreply.github.com>
2024-04-07 13:32:01 +0900

    배너만들기/ 쿼리프로바이더 만들기
    12. (✨2024최신 업데이트) Netflix 만들기
    6강~7강
    
    배너 만들기
    데이터 띄우기(영화 인기 데이터)
    쿼리프로바이더 만들기(앞에세팅해야 했는데 누락)

---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../utils/api";
+const fetchPopularMovies = () => {
+  return api.get(`/movie/popular`);
+};
+
+// 함수로 만들어 놓으면 가져다 쓰기 편함.
+export const usePopularMoviesQuery = () => {
+  return useQuery({
+    queryKey: ["movie-popular"],
+    queryFn: fetchPopularMovies,
+    select: (result) => result.data,
+  });
+};
+
+// hook을 만드는 이유가 뭘까?
+// 나중에 이훅을 다른데서도 사용할 수 있기 때문이다
+// 컴포넌트를 비즈니스 로직과, ui를 분리해주는게 좋음
