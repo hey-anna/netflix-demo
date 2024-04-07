d1a3ab9cc5032a6b6d0de90e1c9e11a8de9b7f6c
hey-anna <106728222+hey-anna@users.noreply.github.com>
2024-04-07 15:11:06 +0900

    배너 디자인하기 및 배너관련 데이터 띄우기

--- src/pages/HomePage/components/Banner/Banner.js
+++ src/pages/HomePage/components/Banner/Banner.js
@@ -1,10 +1,40 @@
 import React from "react";
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
+import Alert from "react-bootstrap/Alert";
+import "./Banner.style.css";
 
 const Banner = () => {
-  const { data } = usePopularMoviesQuery();
-  console.log("## data", data);
-  return <div>Banner</div>;
+  const { data, isLoading, isError, error } = usePopularMoviesQuery();
+
+  // 로딩 스피너 넣기
+  if (isLoading) {
+    <h1>Loding....</h1>;
+  }
+  if (isError) {
+    <Alert variant="danger">{error.message}</Alert>;
+  }
+  const datas = data?.results;
+  // const datas = data && data.results; 위구문 동일 > 데이타가 있으면 데이터를 보여줘
+  if (!datas || datas.length === 0) {
+    return <div>No data found.</div>;
+  }
+  console.log("## data", datas);
+  return (
+    <div
+      className="banner-bg"
+      style={{
+        backgroundImage:
+          "url(" +
+          `https://media.themoviedb.org/t/p/w1066_and_h600_bestv2${datas[0].poster_path}` +
+          ")",
+      }}
+    >
+      <div className="text-white banner-text-area">
+        <h1>{datas[0].title}</h1>
+        <p>{datas[0].overview}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
